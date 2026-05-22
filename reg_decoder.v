13e4d4e256b7359b566b7495473609a5bd714bd6
Liam Farrugia <liamfarrugia2007@outlook.com>
2026-05-22 14:15:28 +1000

    fixed pull issues, now up to date

--- reg_decoder.v
+++ reg_decoder.v
@@ -1,16 +1,16 @@
 module reg_decoder(reg_code, enable);
-  input [2:0] reg_code; // either the register to put onto the bus, or to be loaded into
-  output reg [4:0] enable; 
+  input [2:0] reg_code;
+  output reg [8:0] enable;
 
-  always @(*) begin 
-    case (reg_code) // reg_code comes from fsm 
-      3'b000:  enable = 5'b00001; // r0 is either being loaded to or put on the bus
-      3'b001:  enable = 5'b00010; // r1 same 
-      3'b010:  enable = 5'b00100; // r2 and so on ...
-      3'b011:  enable = 5'b01000; // g
-      3'b100:  enable = 5'b10000; // a
-      default: enable = 5'b00000; 
+  always @(*) begin
+    case (reg_code)
+      3'b000:  enable = 9'b000000001; // r0
+      3'b001:  enable = 9'b000000010; // r1
+      3'b010:  enable = 9'b000000100; // r2
+      3'b011:  enable = 9'b000001000; // g
+      3'b100:  enable = 9'b000010000; // a
+      3'b110:  enable = 9'b000100000; // status
+      default: enable = 0;
     endcase
   end
 endmodule
-
